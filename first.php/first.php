a8c90bca069a437fc5972fde3f55ca230a9254c9
Babusaheb12 <babusahebji4027@gmail.com>
2025-11-22 16:50:49 +0530

    latest code

---
+++ first.php/first.php
@@ -0,0 +1,13 @@
+#!/usr/bin/env php
+<?php
+// Simple Hello World script
+declare(strict_types=1);
+
+// If run from a web server the PHP tag will output directly in the response.
+// If run from CLI the newline makes the output clean.
+echo "Hello, world!\n";
+
+// Exit with success
+exit(0);
+
+?>
